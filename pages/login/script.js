8c35be7ffd6f45af9342825a13a8455edc2f12cf
otashu <otaviocosta23212@gmail.com>
2023-05-10 18:04:59 -0300

    bool logado

--- pages/login/script.js
+++ pages/login/script.js
@@ -14,3 +14,9 @@ document.getElementById('close-menu').addEventListener('click', toggleMenu);
 
 const pagina = document.querySelector('.pagina');
 pagina.style.minHeight = window.innerHeight + 'px';
+
+// Change the class of the material icon based on the isUserLoggedIn variable
+if (isUserLoggedIn) {
+    // Add the class to change the color to red
+    document.querySelector('.material-icons').classList.add('logged-in');
+}
--- pages/login/verify.php
+++ pages/login/verify.php
@@ -12,8 +12,11 @@ $sql = "SELECT * FROM clientes WHERE email = '$email' AND senha = '$senha'";
 
 $resultado = mysqli_query($conn, $sql);
 
+$isUserLoggedIn = false;
+
 if ($resultado && mysqli_num_rows($resultado) > 0) {
     echo "Login bem-sucedido!";
+    $isUserLoggedIn = true;
 } else {
     echo "Email e/ou senha incorretos!";
 }
