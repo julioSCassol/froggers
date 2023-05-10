5d316d2593e66511a5ce6aeca0bb2f1f6a96e159
otashu <otaviocosta23212@gmail.com>
2023-05-10 15:14:39 -0300

    cadastro arrumado

--- pages/cadastro/register.php
+++ pages/cadastro/register.php
@@ -1,38 +1,36 @@
 <?php
 
-$servername = "localhost"; //dps muda essa bomba aq
-$username = "froggers"; 
-$password = "password"; //hackeia eu n
+$servername = "localhost";
+$username = "froggers";
+$password = "password";
 $dbname = "froggers";
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+$nome = $_POST['name'];
 $email = $_POST['email'];
 $senha = $_POST['password'];
+$senha_confirmada = $_POST['confirm_password'];
 
-if(empty($email) || empty($senha)){
+if (empty($nome) || empty($email) || empty($senha) || empty($senha_confirmada)) {
     echo "Por favor preencha todos os campos";
- }else{
-        $sql = "INSERT INTO clientes (email, senha) VALUES ('$email', '$senha')";
-    if ($conn->query($sql) === TRUE) {
-        echo "Registration successful!";
+} else {
+    if ($senha != $senha_confirmada) {
+        echo "As senhas não coincidem";
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        $sql = "INSERT INTO clientes (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
+        if ($conn->query($sql) === TRUE) {
+            echo "Registration successful!";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
     }
- }
- 
-
-
-
-
+}
 
 $conn->close();
-
-
-
-
 
 ?>
