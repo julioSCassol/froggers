7a8e8bc6761d70810d107dd7a7339f25c3007b1c
otashu <otaviocosta23212@gmail.com>
2023-06-18 19:42:32 -0300

    carrinho v1

--- pages/display_cart.php
+++ pages/display_cart.php
@@ -6,13 +6,22 @@ include 'db.php';
 $IDpedido = isset($_SESSION['IDpedido']) ? $_SESSION['IDpedido'] : "";
 $IDcliente = isset($_SESSION['IDcliente']) ? $_SESSION['IDcliente'] : "";
 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+<?php
+
 if (!isset($_SESSION['cart']) || empty($_SESSION['cart'])) {
     echo '<p>O Carrinho está vazio!.</p>';
     exit;
 }
 
 $ids = array_keys($_SESSION['cart']);
-$ids_string = implode(',', $ids);
 
 $stmt = $conn->prepare("SELECT produtos.*, itens_pedido.quantidade, itens_pedido.precoUn
                         FROM produtos
@@ -27,13 +36,21 @@ $result = $stmt->get_result();
 while ($row = $result->fetch_assoc()) {
     $id = $row['id'];
     $quantidade = $_SESSION['cart'][$id]['quantidade'];
-
     echo '<div class="cart-item">';
-    echo '<p>' . $row['nome'] . '</p>';
-    echo '<p>Quantidade: ' . $quantidade . '</p>';
-    echo '<p>Preço: ' . $row['preco'] . '</p>';
-    echo '<button class="remove-item" data-productid="' . $id . '" onclick="removeItemFromCart(this.getAttribute(\'data-productid\'))">Remover</button>';
+    echo '<img src="/assets/camisetas/'. $row['nome'] .'.png" alt="'. $row['nome'] .'" class="catalog-item-img">';
+    echo '<p class = catalog-title>' . $row['nome'] . '</p>';
+    echo '<p class = catalog-price>Preço: ' . number_format($row['preco'], 2) . '</p>';
+    echo '<div class="quantity-box">';
+    echo '<button class="remove-item" data-productid="' . $id . '" onclick="removeItemFromCart(this.getAttribute(\'data-productid\'))">-</button>';
+    echo '<span class="quantity">' . $quantidade . '</span>';
+    echo '<button class="add-item" data-productid="' . $id . '" onclick="addToCart(this.getAttribute(\'data-productid\'))">+</button>';
     echo '</div>';
+    echo '</div>';
+    
 }
-
+// <img src="/assets/camisetas/${produto.nome}.png" alt="${produto.nome}" class="catalog-item-img">
+// </a>            <div class="title-wrapper">
+//   <h3 class="catalog-title">${produto.nome}</h3>
 ?>
+</body>
+</html>
--- pages/remove_from_cart.php
+++ pages/remove_from_cart.php
@@ -6,24 +6,38 @@ include 'db.php';
 $id = isset($_POST['id']) ? $_POST['id'] : "";
 
 $IDpedido = isset($_SESSION['IDpedido']) ? $_SESSION['IDpedido'] : "";
-
 if(array_key_exists($id, $_SESSION['cart'])){
-    unset($_SESSION['cart'][$id]);
+    if ($_SESSION['cart'][$id]['quantidade'] > 1) {
+        $_SESSION['cart'][$id]['quantidade']--;
+        
+        $stmt = $conn->prepare("UPDATE itens_pedido SET quantidade = quantidade - 1 WHERE IDprodutos = ? AND IDpedidos = ?");
+        $stmt->bind_param("ii", $id, $IDpedido);
+        $stmt->execute();
 
-    $stmt = $conn->prepare("DELETE FROM itens_pedido WHERE IDprodutos = ? AND IDpedidos = ?");
-    $stmt->bind_param("ii", $id, $IDpedido);
-    $stmt->execute();
+        if ($stmt->affected_rows > 0) {
+            echo "<script>alert('Quantidade diminuida por 1');</script>";
+        }
+        else {
+            echo "<script>alert('Um erro ocorreu ao diminuir a quantidade');</script>";
+        }
 
-    if ($stmt->affected_rows > 0) {
-        echo "<script>alert('Produto removido do carrinho');</script>";
+    } else {
+        unset($_SESSION['cart'][$id]);
+        $stmt = $conn->prepare("DELETE FROM itens_pedido WHERE IDprodutos = ? AND IDpedidos = ?");
+        $stmt->bind_param("ii", $id, $IDpedido);
+        $stmt->execute();
+
+        if ($stmt->affected_rows > 0) {
+            echo "<script>alert('Produto removido do carrinho');</script>";
+        }
+        else {
+            echo "<script>alert('Erro ao remover produto do carrinho');</script>";
+        }
     }
-    else {
-        echo "<script>alert('Erro ao remover o produto do carrinho');</script>";
-    }
-}
-else {
+} else {
     echo "<script>alert('Produto não encontrado no carrinho');</script>";
 }
 
 $stmt->close();
+
 ?>
